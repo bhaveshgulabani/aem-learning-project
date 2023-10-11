2009490ba1010eefd41dcba28a65770d35b2be09
Bhavesh Gulabani <bhavesh@solvative.com>
2023-10-11 13:36:36 +0530

    add Sightly concepts

---
+++ core/src/main/java/com/learningsite/core/models/MyModel.java
@@ -0,0 +1,57 @@
+package com.learningsite.core.models;
+
+import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+
+import java.util.Optional;
+
+@Model(adaptables = Resource.class)
+public class MyModel {
+
+    @ValueMapValue(name=PROPERTY_RESOURCE_TYPE, injectionStrategy=InjectionStrategy.OPTIONAL)
+    @Default(values="No resourceType")
+    protected String resourceType;
+
+    @SlingObject
+    private Resource currentResource;
+    @SlingObject
+    private ResourceResolver resourceResolver;
+
+    private String title;
+     private String description;
+
+    @PostConstruct
+    protected void init() {
+        PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+        String currentPagePath = Optional.ofNullable(pageManager)
+                .map(pm -> pm.getContainingPage(currentResource))
+                .map(Page::getPath).orElse("");
+
+        title = "My Model Title";
+        description = "My Model Description - \n"
+            + "Resource type is: " + resourceType + "\n"
+            + "Current page is:  " + currentPagePath + "\n";
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return title;
+    }
+
+}
